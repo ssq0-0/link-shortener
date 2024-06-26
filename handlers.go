c6618992aa1cd0b2c33f9109b863190931226ed4
ssq0-0 <snikers1kitkat2@gmail.com>
2024-06-26 22:53:44 +0300

    New vervions Link Shortener. It's not perfect yet, but it's already asynchronous.

--- handlers.go
+++ handlers.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
 	"strings"
 	"sync"
@@ -14,64 +15,92 @@ type SafeWriter struct {
 	Links map[string]string
 }
 
-// var LINKS = make(map[string]string)
+var globalSafeWriter = SafeWriter{Links: make(map[string]string)}
 
-type LinkRequest struct {
-	Link string `json:"link"`
-}
-
-func parseLink(link string) (string, error) {
-	if value, exist := LINKS[link]; exist {
+func (s *SafeWriter) Parser(link string) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	strData := string(link)
+	fmt.Println(strData)
+	if value, exists := s.Links[strData]; exists {
 		return value, nil
 	} else {
-		shortLink, err := coder(link)
+		shortLink, err := coder(strData)
 		if err != nil {
 			return "", err
 		}
-		LINKS[link] = shortLink
+		s.Links[strData] = shortLink
 		return shortLink, nil
 	}
 }
 
+func (s *SafeWriter) CheckerInMap(token string) (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if s.Links == nil {
+		return "", errors.New("Links map is not initialized")
+	}
+	for key, val := range s.Links {
+		if val == token {
+			return key, nil
+		}
+	}
+
+	return "", errors.New("token not found in map")
+}
+
 func RegisterHandlers() {
 	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-		var req LinkRequest
-
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			http.Error(w, "Invalid jsob", http.StatusBadRequest)
-			log.Println("Invalid json")
-			return
-		}
-
-		link := req.Link
-
-		if link == "" {
-			http.Error(w, "Missing link", http.StatusBadRequest)
-			return
-		}
-		result, err := parseLink(link)
+		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, "Error:", http.StatusInternalServerError)
+			http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		}
+		defer r.Body.Close()
+
+		var requestData map[string]interface{}
+		if err := json.Unmarshal(body, &requestData); err != nil {
+			http.Error(w, "Unable to parse JSON", http.StatusBadRequest)
 			return
 		}
-		fmt.Fprintf(w, "Shorted link: http://127.0.0.1:8000/%s", result)
+
+		link, ok := requestData["link"].(string)
+		if !ok {
+			http.Error(w, "Link field is missing or not a string", http.StatusBadRequest)
+			return
+		}
+
+		var wg sync.WaitGroup
+
+		wg.Add(1)
+		go func(link string) {
+			defer wg.Done()
+			response, err := globalSafeWriter.Parser(link)
+			if err != nil {
+				http.Error(w, "Can't short this link", http.StatusBadRequest)
+				return
+			}
+			fmt.Fprintf(w, "Shorted link: http://127.0.0.1:8000/%s", response)
+		}(link)
+		wg.Wait()
+		fmt.Println(globalSafeWriter.Links)
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		token := strings.TrimPrefix(r.URL.Path, "/")
+		var wg sync.WaitGroup
 
-		var originalLink string
-		for key, val := range LINKS {
-			if val == token {
-				originalLink = key
-				break
+		wg.Add(1)
+		go func(token string) {
+			defer wg.Done()
+			response, err := globalSafeWriter.CheckerInMap(token)
+			if err != nil || response == "" {
+				fmt.Println(response)
+				http.Error(w, "ShortLink time expired", http.StatusBadRequest)
+				return
+			} else {
+				http.Redirect(w, r, response, http.StatusFound)
 			}
-		}
-
-		if originalLink != "" {
-			http.Redirect(w, r, originalLink, http.StatusFound)
-		} else {
-			http.Error(w, "Link not found", http.StatusNotFound)
-		}
+		}(token)
+		wg.Wait()
 	})
 }
